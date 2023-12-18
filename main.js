73598e5930c546114842e59734403260be2a9da4
Admin <reanajb@gmail.com>
2023-12-18 12:36:21 -0500

    completed

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 function numberDoubler(num) {
-    // Your code here
-}
+    while(num < 100){
+        num *= 2
+    } return num
+} 
 
 function stringRepeater(str) {
-    // Your code here
+   let str2 = "" 
+    while(str2.length < 10){
+        str2 = str + str2
+    } return str2
 }
 
 function makeDivisible(x, y) {
-    // Your code here
+  
+   while(x % y !== 0){
+    x = x + 1
+   } return x
 }
+
+//check if evenly divisible
+// /while(if not evenly divisible){
+// +1
+// }
 
 module.exports = {
     numberDoubler,
